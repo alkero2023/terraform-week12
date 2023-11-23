c42cb98d43f7ce772e98b51ac5382c86ac11caa3
aly <hollyodoa@gmail.com>
2023-11-22 21:52:01 -0600

    main

---
+++ app5-import-existing/import.tf
@@ -0,0 +1,5 @@
+import {
+  
+  to = aws_instance.demo
+  id = "i-01799282d93ebcdff"
+}
---
+++ app5-import-existing/main.tf
@@ -0,0 +1,19 @@
+
+resource "aws_instance" "demo" {
+  ami                                  = "ami-0230bd60aa48260c6"
+  associate_public_ip_address          = true
+  availability_zone                    = "us-east-1b"
+ 
+  instance_type                        = "t2.micro"
+  ipv6_address_count                   = 0
+  key_name                             = "ec2-demo"
+  monitoring                           = false
+  security_groups                      = ["launch-wizard-5"]
+  subnet_id                            = "subnet-097633aec4f743ef4"
+  tags = {
+    Name = "imported sever"
+    env = "dev"
+    team = "devops"
+  }
+ 
+}
